731dda7ce925e9e7ce4c41dc0b99e12e897f76c5
baseuser <baseuser@jenkinss.dev>
2017-08-01 14:03:36 -0500

    Initial commit

---
+++ src/test/java/banking/Snippet.java
@@ -0,0 +1,11 @@
+package banking;
+
+import org.testng.annotations.Test;
+
+public class Snippet {
+	@Test
+	  public void simplemethod2() {
+		  System.out.println("Testing TestNG Test2");
+	  }
+}
+
